d1c1198aa9907952e47d8660b7ae5d19e387467f
MorkovkAs <avklimakov@gmail.com>
2020-05-20 08:37:06 +0300

    added test for case insensitiveness check

--- report-service/src/test/kotlin/ru/morkovka/report/utils/TaskUtilsTest.kt
+++ report-service/src/test/kotlin/ru/morkovka/report/utils/TaskUtilsTest.kt
@@ -7,28 +7,35 @@ import ru.morkovka.report.utils.TaskUtils.Companion.getProjectNameFromJiraKey
 class TaskUtilsTest {
 
     @Test
-    fun getProjectNameFromJiraKeyTestCorrect() {
+    fun getProjectNameFromJiraKeyTest() {
         val jiraKey = "DM-123"
 
         assertEquals("DM", getProjectNameFromJiraKey(jiraKey))
     }
 
     @Test
-    fun getProjectNameFromJiraKeyTestCorrect2() {
+    fun getProjectNameFromJiraKeyTestCheckCaseInsensitiveness() {
+        val jiraKey = "Dm-123"
+
+        assertEquals("Dm", getProjectNameFromJiraKey(jiraKey))
+    }
+
+    @Test
+    fun getProjectNameFromJiraKeyTestMultipleHyphens() {
         val jiraKey = "DM-NEW-123"
 
         assertEquals("DM-NEW", getProjectNameFromJiraKey(jiraKey))
     }
 
     @Test
-    fun getProjectNameFromJiraKeyTestCorrect3() {
+    fun getProjectNameFromJiraKeyTestCorrectNameWithNumbers() {
         val jiraKey = "D12M-123"
 
         assertEquals("D12M", getProjectNameFromJiraKey(jiraKey))
     }
 
     @Test
-    fun getProjectNameFromJiraKeyTestCorrect4() {
+    fun getProjectNameFromJiraKeyTestCorrectNameWithNumbers2() {
         val jiraKey = "D1M2-123"
 
         assertEquals("D1M2", getProjectNameFromJiraKey(jiraKey))
